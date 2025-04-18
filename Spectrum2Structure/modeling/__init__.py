6aea9abf71cab6340193c81c36613cbea5f3fb9d
Xianqi Deng <xdeng5@albany.edu>
2025-04-17 20:26:05 -0400

    Delete Spectrum2Structure directory

--- Spectrum2Structure/modeling/__init__.py
+++
@@ -1,6 +0,0 @@
-from modeling.attention import (Past, BaseAttention, MultiHeadAttention,
-                                     AttentionLayer)
-from modeling.embedding import PositionalEmbedding, TokenEmbedding
-from modeling.feedforward import Swish, PositionwiseFeedForward
-from modeling.masking import PadMasking, FutureMasking
-from modeling.transformer import Transformer
